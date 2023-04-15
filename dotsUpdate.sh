6de504466c8ff9bf4f465414eddaa7174adfd333
CryZFix <cryzfix@gmail.com>
2023-04-15 13:43:13 +0400

    More not needed

--- dotsUpdate.sh
+++
@@ -1,9 +0,0 @@
-dotdir='$HOME/.dotfiles'
-
-mkdir -p $HOME/.config
-ln -sf $dotdir/.config/bspwm $HOME/.config/bspwm
-ln -sf $dotdir/.local $HOME/.local
-ln -sf $dotdir/.p10k.zsh $HOME/.p10k.zsh
-ln -sf $dotdir/.xinitrc $HOME/.xinitrc
-ln -sf $dotdir/.zprofile $HOME/.zprofile
-ln -sf $dotdir/.zshrc $HOME/.zshrc
